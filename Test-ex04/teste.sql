0d5aba0985aefc836c80b3ef147a0ffe74c983d9
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-01-30 20:18:58 -0300

    fimAula017

---
+++ Test-ex04/teste.sql
@@ -0,0 +1,22 @@
+/* 
+    CREATE DATABASE sistemaDeCadastro;
+    USE sistemaDeCadastro;
+    SHOW TABLES;
+        CREATE TABLE usuarios(
+        nome VARCHAR(50),
+        email VARCHAR(100),
+        idade INT
+); */
+
+/* INSERT INTO usuarios(idade, email, nome) VALUES(
+    8,
+    'email@teste3.com',
+    'Maria Clara'
+); */
+
+/* SELECT * FROM usuarios WHERE idade = 8; */
+/* SELECT * FROM usuarios WHERE nome = 'Victor Lima'; */
+/* SELECT * from usuarios WHERE idade >= 18; */
+
+/* DELETE FROM usuarios WHERE nome = 'Josevaldo'; */
+/* UPDATE usuarios SET email = 'email@teste2.com' WHERE nome = 'Maria Clara'; */
